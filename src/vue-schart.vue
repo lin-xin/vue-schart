c4dc3406d43456488a5d49c5b4ba20a62227abf0
lin-xin <2981207131@qq.com>
2017-05-27 17:56:34 +0800

    Add vue-schart v0.1.1

---
+++ src/vue-schart.vue
@@ -0,0 +1,44 @@
+<template>
+    <div>
+        <canvas :id="canvasId" :width="width" :height="height"></canvas>
+    </div>
+</template>
+
+<script>
+    import sChart from 'schart.js';
+    export default {
+        data() {
+            return {}
+        },
+        props: {
+            canvasId: {
+                type: String,
+                default: ''
+            },
+            width: {
+                type: [String, Number],
+                default: 500
+            },
+            height: {
+                type: [String, Number],
+                default: 400
+            },
+            type: {
+                type: String,
+                default: 'bar',
+            },
+            data: {
+                type: Array,
+                default: [],
+            },
+            options: {
+                type: Object,
+                required: false
+            }
+        },
+        mounted() {
+            var self = this;
+            new sChart(self.canvasId, self.type, self.data, self.options);
+        }
+    }
+</script>
